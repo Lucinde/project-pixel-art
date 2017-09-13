b9c4482311435f7716196cbc603252e478352cc3
Lucinde <lucinde@macbook-lucinde.local>
2017-09-13 16:50:09 +0200

    added canvas

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
 // Select color input
-// Select size input
+
+// define submit button action
+$("#submit").click(makeGrid);
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
+  // select sizes
+  var height = $("#input_height").val();
+  var width = $("#input_width").val();
 
-// Your code goes here!
+  // draw table
+  var i;
+  var j;
+  var tr = "";
+  //create row
+  for (i = 0; i < height; i++) {
+    tr += "<tr>";
+    //create column
+    for (j = 0; j < width; j++) {
+      tr += "<td>&nbsp;</td>";
+    }
+    tr += "</tr>";
+  }
+  $("#pixel_canvas").html(tr);
+  $("td").click(colorPixel);
+}
 
+function colorPixel() {
+  alert("color");
 }
